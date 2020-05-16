efe0addadd0f248f5d50cb81f43df4806c8b0191
dong heon cho <davinnovation@gmail.com>
2020-05-17 02:05:25 +0900

    add fd + bokeh plot

--- setup.py
+++ setup.py
@@ -118,7 +118,8 @@ setuptools.setup(
     # You can install these using the following syntax, for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'plotting':  ['matplotlib'],
+        'plotting':  ['matplotlib', 'bokeh'],
+        'data': ['finance-datareader']
     },
 
     # If there are data files included in your packages that need to be
